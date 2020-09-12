5783144054825ed991b7fc5e2af45d2a78779fc9
jaharris <jharris@humnet.ucla.edu>
2020-09-12 10:18:38 -0700

    Update

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 # hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-hugo -t academic
+# hugo -t academic
+hugo
 
 
 # Go To Public folder
